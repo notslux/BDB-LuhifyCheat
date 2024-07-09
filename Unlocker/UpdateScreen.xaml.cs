37abec329a27bc8e0ca18c268a4ffd5ba4de91a6
Slux <142702648+notslux@users.noreply.github.com>
2024-07-09 02:36:27 -0500

    Add files via upload

---
+++ Unlocker/UpdateScreen.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+namespace FortniteBurger
+{
+    public partial class UpdateScreen : Page
+    {
+        public UpdateScreen()
+        {
+            InitializeComponent();
+        }
+
+        internal async void CheckForUpdate()
+        {
+            MainWindow.main.TotalFrame.Content = this;
+
+            UpdateText.Text = "Checking for updates...";
+            Spinner.Visibility = Visibility.Visible;
+            await Task.Delay(1000);
+            MainWindow.AutoUpdater.CheckForUpdates();
+        }
+
+        internal async void NoUpdate()
+        {
+            UpdateText.Text = "No updates found";
+            Spinner.Visibility = Visibility.Hidden;
+            Check.Visibility = Visibility.Visible;
+            await Task.Delay(1000);
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                MainWindow.main.TotalFrame.Content = null;
+            }));
+            MainWindow.main.UpdateCheckDone();
+        }
+
+        internal async void FailUpdate()
+        {
+            UpdateText.Text = "Could not check for updates";
+            Spinner.Visibility = Visibility.Hidden;
+            Error.Visibility = Visibility.Visible;
+            await Task.Delay(1000);
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                MainWindow.main.TotalFrame.Content = null;
+            }));
+            MainWindow.main.UpdateCheckDone();
+        }
+
+        internal void FoundUpdate(string Version)
+        {
+            UpdateText.Text = "Found Update: v" + Version;
+        }
+    }
+}
